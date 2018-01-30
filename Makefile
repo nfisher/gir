7586dac7c1c583713e3cf2f5db5579a4ed533030
Nathan Fisher <nfisher@junctionbox.ca>
2018-01-29 23:48:52 -0300

    Add golint and coverage
     - add output files for vet, lint, and coverage.

--- Makefile
+++ Makefile
@@ -1,12 +1,29 @@
 SHELL := /bin/sh
 
+SRC := $(wildcard *.go)
+
 .PHONY: all
-all:
-	ragel -G2 -Z graphql_collections.rl 
+all: lint.out vet.out coverage.out 
+
+.PHONY: install
+install: graphql_collections.go $(SRC)
 	go install -v
-	go vet -v
-	go test -v
+
+cover.out: $(SRC)
+	go test -v -cover -covermode atomic -coverprofile cover.out .
+
+coverage.out: cover.out
+	go tool cover -func=cover.out | tee coverage.out
+
+vet.out: install
+	go vet -v | tee vet.out
+
+lint.out: install
+	golint | tee lint.out
 
 .PHONY: clean
 clean:
 	go clean -i ./...
+
+graphql_collections.go: graphql_collections.rl
+	ragel -G2 -Z graphql_collections.rl
--- emitter.go
+++ emitter.go
@@ -1,7 +1,9 @@
 package gir
 
+// GType is an enum type for classes of tokens.
 type GType int
 
+// Token contains the position and type of emitted lexical token.
 type Token struct {
 	Start int
 	End   int
@@ -9,18 +11,28 @@ type Token struct {
 	Data  []byte
 }
 
+// Emitter provides a common interface for emitting lexical tokens.
 type Emitter interface {
 	Emit(start int, end int, gtype GType, d []byte)
 }
 
 const (
+	// Undefined is a token that is undefined based on lexical analysis.
 	Undefined GType = iota
+	// BooleanValue is the enum for a boolean token.
 	BooleanValue
+	// EnumValue is the enum for a enum token.
 	EnumValue
+	// FloatValue is the enum for a float token.
 	FloatValue
+	// IntValue is the enum for a int token.
 	IntValue
+	// Name is the enum for a name token.
 	Name
+	// NullValue is the enum for a null token.
 	NullValue
+	// StringValue is the enum for a string token.
 	StringValue
+	// Variable is the enum for a variable token.
 	Variable
 )
